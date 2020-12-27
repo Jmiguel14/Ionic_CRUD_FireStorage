dd0d17188d049577fa4312fea6a06e0f6d54450d
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-12-26 22:09:07 -0500

    Ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmXRvJabGZ-XlRJEureecYRe8qDO0Rkl0",
+    authDomain: "crudapp2020b.firebaseapp.com",
+    projectId: "crudapp2020b",
+    storageBucket: "crudapp2020b.appspot.com",
+    messagingSenderId: "152438789445",
+    appId: "1:152438789445:web:d0434f1190e62c5dad5a2c",
+    measurementId: "G-11WDFQGYGG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmXRvJabGZ-XlRJEureecYRe8qDO0Rkl0",
+    authDomain: "crudapp2020b.firebaseapp.com",
+    projectId: "crudapp2020b",
+    storageBucket: "crudapp2020b.appspot.com",
+    messagingSenderId: "152438789445",
+    appId: "1:152438789445:web:d0434f1190e62c5dad5a2c",
+    measurementId: "G-11WDFQGYGG"
+  }
 };
 
 /*
